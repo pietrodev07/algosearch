aeefb57fb62c43a75f94150fddc92367d2578197
pietrodev07 <pietro.dev.07@gmail.com>
2024-10-12 10:30:38 +0200

    feat(searching): add binary-search algorithm

---
+++ searching/binary_search.go
@@ -0,0 +1,18 @@
+package searching
+
+// BinarySearch implements the binary search algorithm.
+// The array must be sorted for binary search to work.
+func BinarySearch(arr []int, target int) int {
+	left, right := 0, len(arr)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
