6649431d578ec220bffe61e8dd4fe54d6dbd83e2
pietrodev07 <pietro.dev.07@gmail.com>
2024-10-12 10:32:27 +0200

    feat(playground): add examples of searching in the main file

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"algosearch/searching"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello from algosearch")
+	arr := []int{10, 20, 30, 40, 50}
+	target := 30
+
+	// Linear Search
+	fmt.Printf("Array: %v\n", arr)
+	fmt.Printf("Linear Search for %d: index %d\n", target, searching.LinearSearch(arr, target))
+
+	// Binary Search
+	fmt.Printf("Binary Search for %d: index %d\n", target, searching.BinarySearch(arr, target))
 }
